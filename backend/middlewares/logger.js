e156677e69ed2add8d1daddfc201087694dca067
Jazzyvesper <jazzyvesper@yandex.ru>
2021-08-24 15:58:17 +0300

    исправлена синт ошибка

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const expressWinston = require('express-winston');
 
 
-/ создадим логгер запросов
+// создадим логгер запросов
 const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.File({ filename: 'request.log' }),
